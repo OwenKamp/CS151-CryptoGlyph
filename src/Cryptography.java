297136b9e6fd8d6a662cc4ae0fb9f36f138ae62b
davidwarshawsky <davidawarshawsky@gmail.com>
2022-11-26 15:42:28 -0800

    Knapsack Works

--- src/Cryptography.java
+++ src/Cryptography.java
@@ -1,7 +1,6 @@
 public abstract class Cryptography {
     public String plaintext;
     public String ciphertext;
-
     public abstract String encrypt(String plaintext, String key);
     public abstract String decrypt(String ciphertext, String key);
 }
--- src/Knapsack.java
+++ src/Knapsack.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Knapsack extends Cryptography {
-//	m and n are relatively prime
 //	n is greater than the sum of all elements in the super- increasing knapsack.
 //	To get the general knapsack, you need to mod the result of sik and multiplier(m) by n.  
 	private static Random r = new Random();
@@ -10,7 +9,11 @@ public class Knapsack extends Cryptography {
 //	https://www.geeksforgeeks.org/copyright-information/?ref=footer
 //	https://www.geeksforgeeks.org/java-program-to-display-all-prime-numbers-from-1-to-n/
 	public static float generateMultiplier() {
+<<<<<<< Updated upstream
 		return r.nextInt(1, 4) + r.nextFloat() + 0.01f;
+=======
+		return r.nextInt(1, 3) + (r.nextFloat() * 0.5f) + 0.01f;
+>>>>>>> Stashed changes
 	}
 
 //	For the Super-Increasing Knapsack generate an int array where the next element
@@ -27,7 +30,15 @@ public class Knapsack extends Cryptography {
 ////	times a float between 1.01 and 4.00 times the sum. Then it should be cast to an int.
 		for (int i = 1; i < 8; i++) {
 			float mul = generateMultiplier();
+<<<<<<< Updated upstream
 			sik[i] = (int) (sum * mul);
+=======
+			long nextNum = (long) (sum * mul);
+			while (nextNum == sik[i - 1] || nextNum <= sum) {
+				nextNum += 1;
+			}
+			sik[i] = nextNum;
+>>>>>>> Stashed changes
 			sum += sik[i];
 //			System.out.println(sum);
 		}
@@ -82,8 +93,36 @@ public class Knapsack extends Cryptography {
 	
 	@Override
 	public String encrypt(String plainText, String key) {
+<<<<<<< Updated upstream
 		String binary = convertStringToBinary(plainText);
 		return null;
+=======
+		if (plainText == null) {
+			return null;
+		}
+		long len = plainText.length();
+		if (len == 0) {
+			return null;
+		}
+		long[] keyArray = csvToLongArray(key);
+
+		long[] sums = new long[(int) len];
+
+//		For each letter in the plainText to encrypt 
+		for (int i = 0; i < len; i++) {
+//			get that letter as a binary 8-bit representation
+			byte c = (byte) plainText.charAt(i);
+//			for each bit in each letter starting right to left,
+			for (int j = 0; j < 8; j++) {
+//				if the bit is 1,
+				if (getBit(c, j) == 1) {
+//					add to the current letter's sum the corresponding keyArray's value.
+					sums[i] += keyArray[7 - j];
+				}
+			}
+		}
+		return longArrayToCsv(sums);
+>>>>>>> Stashed changes
 	}
 
 	@Override
@@ -98,6 +137,7 @@ public class Knapsack extends Cryptography {
 	public static int[] charToBinaryArray(char c) {
 		int[] bArray = new int[8];
 		String charBinaryStr = charToBinaryStr(c);
+<<<<<<< Updated upstream
 		for (int i=0;i<8;i++) {
 			bArray[i] = Integer.parseInt(charBinaryStr[i]);
 		}
@@ -107,6 +147,81 @@ public class Knapsack extends Cryptography {
 	public static void main(String[] args) {
 //		System.out.println(Arrays.toString(generateSIK()));
 		System.out.println();
+=======
+		for (int i = 0; i < 8; i++) {
+			binaryArray[i] = Integer.parseInt(Character.toString(charBinaryStr.charAt(i)));
+		}
+		return binaryArray;
+	}
+
+//	https://stackoverflow.com/questions/9354860/how-to-get-the-value-of-a-bit-at-a-certain-position-from-a-byte
+	public static int getBit(byte b, int position) {
+		return (b >> position) & 1;
+	}
+
+	public static char solveKnapsack(long encrypted, long[] sik) {
+		byte ret = 0x00;
+		long m = sik[8];
+		long n = sik[9];
+		long conversionFactor = MultiplicativeInverse.mult_inverse_nm(n, m);
+		long sum = (encrypted * conversionFactor) % n;
+		int i = 0;
+		while(sum > 0) {
+			if (sum >= sik[7-i]) {
+				long v = sik[7-i];
+				ret = (byte)(ret | MASKS[i]);
+				sum = sum - v;
+			}
+			i++;
+		}
+		return (char) ret;
+	}
+
+	public static String longArrayToCsv(long[] longArray) {
+		StringBuilder sb = new StringBuilder();
+		for (long i : longArray) {
+			sb.append(i);
+			sb.append(",");
+		}
+		sb.deleteCharAt(sb.length() - 1);
+		return sb.toString();
+	}
+
+	public static long[] csvToLongArray(String csv) {
+		return Stream.of(csv.split(",")).mapToLong(Long::parseLong).toArray();
+	}
+
+	public static String convertStringToBinary(String input) {
+
+		StringBuilder result = new StringBuilder();
+		char[] chars = input.toCharArray();
+		for (char aChar : chars) {
+			result.append(charToBinaryStr(aChar));
+		}
+		return result.toString();
+
+	}
+
+	public static String charToBinaryStr(char c) {
+		String charS = String.format("%8s", Integer.toBinaryString(c)).replaceAll(" ", "0");  
+		return charS.substring(charS.length()-8, charS.length());
+	}
+
+	public static void main(String[] args) {
+		Knapsack k = new Knapsack();
+		long[] sik = Knapsack.generateSIK();
+		long[] gk = Knapsack.generateGK(sik);
+		String wordToEncrypt = "Hello There Warshawsky";
+		System.out.println(wordToEncrypt);
+		String encrypted = k.encrypt(wordToEncrypt, Knapsack.longArrayToCsv(gk));
+		System.out.println(encrypted);
+		String decrypted = k.decrypt(encrypted, Knapsack.longArrayToCsv(sik));
+		System.out.println(decrypted);
+
+//		System.out.println(Knapsack.solveKnapsack(encrypted[0],sik));
+//		https://stackoverflow.com/questions/36019032/how-to-iterate-over-all-bits-of-a-byte-in-java
+
+>>>>>>> Stashed changes
 	}
 
 }
