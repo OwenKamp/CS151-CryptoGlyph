1de38ee44ef530bb9da990671a1024eee27e54f6
your.user.name <davidawarshawsky@gmail.com>
2022-11-27 19:32:57 -0800

    Let's keep track of different screens. Like a JPanel but for directly applying to JFrame. Has show() and hide() methods

---
+++ src/AlgorithmScreen.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+public class AlgorithmScreen{
+	private JFrame f;
+	private ArrayList<JComponent> componentList = new ArrayList<>();
+	
+	public AlgorithmScreen(JFrame f) {
+		this.f = f;
+	}
+	
+//	Add components the order they will be placed on top of each other. First one is front layer, second is behind.
+	public void add(JComponent c) {
+		componentList.add(c);
+	}
+	
+	public void hide() {
+		for (JComponent c: componentList) {
+			f.remove(c);
+		}
+		f.revalidate();
+		f.repaint();
+	}
+	
+	public void show() {
+		for (JComponent c: componentList) {
+			f.add(c);
+		}
+		f.revalidate();
+		f.repaint();
+	}
+}
